b61517c24c3848402bc82d50930386cbd1aaf52b
israelbraga2 <ibraga1003@gmail.com>
2022-11-16 17:11:56 -0300

    Exceção para objeto não encontrado

--- src/main/java/com/israel/helpdesk/services/TecnicoService.java
+++ src/main/java/com/israel/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.israel.helpdesk.domain.Tecnico;
 import com.israel.helpdesk.repositories.TecnicoRepository;
+import com.israel.helpdesk.services.exceptions.ObjectnotFoundException;
 
 @Service
 public class TecnicoService {
@@ -16,6 +17,7 @@ public class TecnicoService {
 	
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(()->
+		new ObjectnotFoundException("Objeto não encontrado! Id: " + id));
 	}
 }
