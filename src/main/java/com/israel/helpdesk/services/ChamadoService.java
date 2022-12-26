ace8f918bec1084cd20b997b7f0b4560efcb40f5
israelbraga2 <ibraga1003@gmail.com>
2022-12-26 15:37:57 -0300

    Update para chamado implementado

--- src/main/java/com/israel/helpdesk/services/ChamadoService.java
+++ src/main/java/com/israel/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.israel.helpdesk.services;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -40,6 +41,14 @@ public class ChamadoService {
 	public Chamado create(@Valid ChamadoDTO objDTO) {
 		return repository.save(newChamado(objDTO));
 	}
+	
+	public Chamado update(Integer id, @Valid ChamadoDTO objDTO) {
+		objDTO.setId(id);
+		Chamado oldObj = findById(id);
+		oldObj = newChamado(objDTO);
+		return repository.save(oldObj);
+	}
+	
 
 	private Chamado newChamado(ChamadoDTO obj) {
 		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
@@ -49,6 +58,11 @@ public class ChamadoService {
 		if(obj.getId()!= null) {
 			chamado.setId(obj.getId());
 		}
+		
+		if(obj.getStatus().equals(2)) {
+			chamado.setDataFechamento(LocalDate.now());
+		}
+		
 		chamado.setTecnico(tecnico);
 		chamado.setCliente(cliente);
 		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
@@ -57,4 +71,6 @@ public class ChamadoService {
 		chamado.setObservacoes(obj.getObservacoes());
 		return chamado;
 	}
+
+	
 }
